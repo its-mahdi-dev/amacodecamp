b931771aa03a4803bd0c0c81bcb65e5d39d4b3bf
its-mahdi-dev <dev@itsmahdi.com>
2025-05-12 02:07:27 +0330

    fixup! change layout

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -61,13 +61,13 @@ class AuthController extends Controller
             ]
         ];
 
-        // $response = SmsIr::verifySend($phone, $templateId, $parameters, [
-        //     CURLOPT_SSL_VERIFYPEER => false,
-        //     CURLOPT_CAINFO => "D:/wamp64/bin/php/php8.2.0/cacert.pem"
-        // ]);
+        $response = SmsIr::verifySend($phone, $templateId, $parameters, [
+            CURLOPT_SSL_VERIFYPEER => false,
+            // CURLOPT_CAINFO => "D:/wamp64/bin/php/php8.2.0/cacert.pem"
+        ]);
 
 
-        // $url = "https://api.sms.ir/v1/send?username=09304500129&Password=Amir#1234&Line=&mobile=09369488096&text=1234";
+        // $url = "https://api.sms.ir/v1/send?username=09304500129&Password=Amir#1234&Line=&mobile=$phone&text=$otpCode";
         return Response::success(null,ResponseMessages::OTP_SENT_SUCCESS);
     }
 
