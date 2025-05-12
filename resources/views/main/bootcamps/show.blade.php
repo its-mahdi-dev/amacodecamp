22bed0bb1fe30e44fd8b8217de8be47c185c8fa3
its-mahdi-dev <dev@itsmahdi.com>
2025-05-12 11:29:23 +0330

    fixup! change layout

--- resources/views/main/bootcamps/show.blade.php
+++ resources/views/main/bootcamps/show.blade.php
@@ -718,13 +718,13 @@
                                 </li>
                                 <li class="d-flex align-items-center justify-content-between">
                                     <span><i class="la la-users me-2 text-color"></i>دانشجو</span>
-                                    ${data.student_count}
+                                    ${data.students_count}
                                 </li>
                                 <li class="d-flex align-items-center justify-content-between">
                                     <span><i class="la la-users me-2 text-color"></i>زمان شروع</span>
                                     ${data.start_time}
                                 </li>
-                                
+
                                 <li class="d-flex align-items-center justify-content-between">
                                     <span><i class="la la-certificate me-2 text-color"></i>مدرک</span>${data.certification ? 'دارد' : 'ندارد'}
                                 </li>
